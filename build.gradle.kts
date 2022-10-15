daad39d65bb2a3676db1dd43013d9383164af64d
John Turkson <git@johnturkson.com>
2022-10-14 21:15:40 -0700

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application") version "8.0.0-alpha03" apply false
-    id("com.android.library") version "8.0.0-alpha03" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10" apply false
+    id("com.android.application") version "8.0.0-alpha04" apply false
+    id("com.android.library") version "8.0.0-alpha04" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.20" apply false
     id("com.google.dagger.hilt.android") version "2.41" apply false
 }
