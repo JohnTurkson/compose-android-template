e3afaac65f86a84e4612f3ff29c487be36c420ab
John Turkson <git@johnturkson.com>
2022-10-14 21:24:01 -0700

    Configure compose compiler via `composeOptions`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
     }
     
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.1"
+        kotlinCompilerExtensionVersion = "1.3.2"
     }
     
     packagingOptions {
@@ -56,7 +56,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.activity:activity-compose:1.6.0")
     
-    implementation("androidx.compose.compiler:compiler:1.3.2")
     implementation("androidx.compose.ui:ui:1.3.0-rc01")
     implementation("androidx.compose.ui:ui-tooling-preview:1.3.0-rc01")
     implementation("androidx.compose.material3:material3:1.0.0-rc01")
