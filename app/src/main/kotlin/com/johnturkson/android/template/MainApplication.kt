a7015f5771e0ae6f9552dfa291d6485fb4d31eb7
John Turkson <git@johnturkson.com>
2022-11-23 00:04:18 -0800

    Reformat `MainApplication`

--- app/src/main/kotlin/com/johnturkson/android/template/MainApplication.kt
+++ app/src/main/kotlin/com/johnturkson/android/template/MainApplication.kt
@@ -8,7 +8,6 @@ import javax.inject.Inject
 
 @HiltAndroidApp
 class MainApplication : Application(), Configuration.Provider {
-    
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
     
