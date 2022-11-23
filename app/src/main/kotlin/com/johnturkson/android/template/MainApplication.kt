9eb09e3f08eb0e1f68331f55a9cff80e2d213a33
John Turkson <git@johnturkson.com>
2022-11-23 00:47:49 -0800

    Add camerax configuration

--- app/src/main/kotlin/com/johnturkson/android/template/MainApplication.kt
+++ app/src/main/kotlin/com/johnturkson/android/template/MainApplication.kt
@@ -1,19 +1,31 @@
 package com.johnturkson.android.template
 
 import android.app.Application
+import android.util.Log
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.CameraXConfig
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 @HiltAndroidApp
-class MainApplication : Application(), Configuration.Provider {
+class MainApplication : Application(), Configuration.Provider, CameraXConfig.Provider {
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
     
     override fun getWorkManagerConfiguration(): Configuration {
         return Configuration.Builder()
             .setWorkerFactory(workerFactory)
+            .build()
+    }
+    
+    override fun getCameraXConfig(): CameraXConfig {
+        return CameraXConfig.Builder
+            .fromConfig(Camera2Config.defaultConfig())
+            .setAvailableCamerasLimiter(CameraSelector.DEFAULT_BACK_CAMERA)
+            .setMinimumLoggingLevel(Log.ERROR)
             .build()
     }
 }
